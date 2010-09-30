20a59418e394b7be403c3a994c8608446a6a1567
thiagovaguiar@yahoo.com.br <thiagovaguiar@yahoo.com.br@bb2e0003-1184-2d7a-783e-2f35ea7fb7e2>
2010-09-30 01:39:31 +0000

    Primeiro esqueleto do aplicativo.

--- CloudWB/CloudWB/src/com/cloudwb/project/client/CloudWB.java
+++ CloudWB/CloudWB/src/com/cloudwb/project/client/CloudWB.java
@@ -1,152 +1,89 @@
 package com.cloudwb.project.client;
 
-import com.cloudwb.project.shared.FieldVerifier;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.TabPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.RootPanel;
 
-/**
- * Entry point classes define <code>onModuleLoad()</code>.
- */
 public class CloudWB implements EntryPoint {
-	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
-	 */
-	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network "
-			+ "connection and try again.";
 
-	/**
-	 * Create a remote service proxy to talk to the server-side Greeting service.
-	 */
-	private final GreetingServiceAsync greetingService = GWT
-			.create(GreetingService.class);
-
-	/**
-	 * This is the entry point method.
-	 */
+	private VerticalPanel mainPanel = new VerticalPanel();
+	private FlexTable stocksFlexTable = new FlexTable();
+	private HorizontalPanel addPanel = new HorizontalPanel();
+	private Label lastUpdatedLabel = new Label();
+	
 	public void onModuleLoad() {
-		final Button sendButton = new Button("Send");
-		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
-		final Label errorLabel = new Label();
+		// Assemble Main panel.
+		mainPanel.add(stocksFlexTable);
+		mainPanel.add(addPanel);
+		mainPanel.add(lastUpdatedLabel);
 
-		// We can add style names to widgets
-		sendButton.addStyleName("sendButton");
+		// Associate the Main panel with the HTML host page.
+		RootPanel.get("stockList").add(mainPanel);
+		
+		// Create a tab panel
+		TabPanel tabPanel = new TabPanel();
 
-		// Add the nameField and sendButton to the RootPanel
-		// Use RootPanel.get() to get the entire body element
-		RootPanel.get("nameFieldContainer").add(nameField);
-		RootPanel.get("sendButtonContainer").add(sendButton);
-		RootPanel.get("errorLabelContainer").add(errorLabel);
+		// Set the width to 400 pixels
+		tabPanel.setWidth("400px");
 
-		// Focus the cursor on the name field when the app loads
-		nameField.setFocus(true);
-		nameField.selectAll();
+		// Adiciona Tab Principal
+		HTML homeText = new HTML("Informacoes gerais sobre o site<br>"+
+				"<FORM METHOD=POST NAME=dados>" +
+				"<P>Nome do Site: <INPUT TYPE=TEXT NAME=nomeSite SIZE=10 MAXLENGTH=8></P>" +
+				"<P>Titulo: <INPUT TYPE=TEXT NAME=tituloSite SIZE=20 MAXLENGTH=8></P>" +
+				"<P>Banner: <INPUT TYPE=TEXT NAME=nomeSite SIZE=20 MAXLENGTH=8>" +
+				"<INPUT TYPE=SUBMIT VALUE=upload ONCLICK=\"Javascript:alert('buscando imagem!');\"></P>" +
+				"<INPUT TYPE=SUBMIT VALUE=SEGUIR ONCLICK=\"Javascript:alert('O form sera enviado!');\">" +
+				"<INPUT TYPE=RESET VALUE=LIMPAR></P>");
+		tabPanel.add(homeText, "DADOS PRINCIPAIS");
+		
+		// Adiciona Tab Layout
+		HTML layoutText = new HTML("Escolha o Layout das paginas:<br>"+
+				"<FORM METHOD=POST NAME=layout>" +
+				"<P>Opcoes de layout:</P>" +
+				"<P><INPUT TYPE=CHECKBOX NAME=op1>opcao 1" +
+				"<INPUT TYPE=CHECKBOX NAME=op2>opcao 2</P>" +
+				"<P><INPUT TYPE=CHECKBOX NAME=op3>opcao 3" +
+				"<INPUT TYPE=CHECKBOX NAME=op4>opcao 4</P><P>" +
+				"<INPUT TYPE=SUBMIT VALUE=SEGUIR ONCLICK=\"Javascript:alert('O form sera enviado!');\">" +
+				"<INPUT TYPE=RESET VALUE=LIMPAR></P>");
+		tabPanel.add(layoutText, "ESTRUTURA DO SITE");
 
-		// Create the popup dialog box
-		final DialogBox dialogBox = new DialogBox();
-		dialogBox.setText("Remote Procedure Call");
-		dialogBox.setAnimationEnabled(true);
-		final Button closeButton = new Button("Close");
-		// We can set the id of a widget by accessing its Element
-		closeButton.getElement().setId("closeButton");
-		final Label textToServerLabel = new Label();
-		final HTML serverResponseLabel = new HTML();
-		VerticalPanel dialogVPanel = new VerticalPanel();
-		dialogVPanel.addStyleName("dialogVPanel");
-		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-		dialogVPanel.add(textToServerLabel);
-		dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-		dialogVPanel.add(serverResponseLabel);
-		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-		dialogVPanel.add(closeButton);
-		dialogBox.setWidget(dialogVPanel);
+		// Adiciona Tab Template
+		HTML templantText = new HTML("Escolha de um Templante<br>"+
+				"<FORM METHOD=POST NAME=templante>" +
+				"<P>Opcoes de templante:</P>" +
+				"<P><INPUT TYPE=CHECKBOX NAME=op1>opcao 1" +
+				"<INPUT TYPE=CHECKBOX NAME=op2>opcao 2</P>" +
+				"<P><INPUT TYPE=CHECKBOX NAME=op3>opcao 3" +
+				"<INPUT TYPE=CHECKBOX NAME=op4>opcao 4</P><P>" +
+				"<INPUT TYPE=SUBMIT VALUE=SEGUIR ONCLICK=\"Javascript:alert('O form sera enviado!');\">" +
+				"<INPUT TYPE=RESET VALUE=LIMPAR></P>");
+		tabPanel.add(templantText, "TEMPLANTE");
+		
+		// Adiciona Tab Módulo
+		HTML modulText = new HTML("Formulario para a escolha dos modulos");
+		tabPanel.add(modulText, "MODULOS");
+		
+		// Adiciona Exemplo
+		HTML exemplForm = new HTML("<FORM METHOD=POST NAME=contato>" +
+				" <P>Nome: <INPUT TYPE=TEXT NAME=USUARIO SIZE=10 MAXLENGTH=8></P>" +
+				"<P>Sobre quais assuntos voce gostaria de saber mais?</P>" +
+				"<P><INPUT TYPE=CHECKBOX NAME=ESPORTE>Esportes</P>" +
+				"<P><INPUT TYPE=CHECKBOX NAME=CULTURA>Cultura</P>" +
+				"<P><INPUT TYPE=CHECKBOX NAME=CULINARIA>Culinaria</P>" +
+				"<P><INPUT TYPE=CHECKBOX NAME=INFO>Informatica</P><P>" +
+				"<INPUT TYPE=SUBMIT VALUE=Enviar ONCLICK=\"Javascript:alert('O form sera enviado!');\">" +
+				"<INPUT TYPE=RESET VALUE=Limpar></P>");
+		tabPanel.add(exemplForm, "Exemplo Formulario");
 
-		// Add a handler to close the DialogBox
-		closeButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				dialogBox.hide();
-				sendButton.setEnabled(true);
-				sendButton.setFocus(true);
-			}
-		});
-
-		// Create a handler for the sendButton and nameField
-		class MyHandler implements ClickHandler, KeyUpHandler {
-			/**
-			 * Fired when the user clicks on the sendButton.
-			 */
-			public void onClick(ClickEvent event) {
-				sendNameToServer();
-			}
-
-			/**
-			 * Fired when the user types in the nameField.
-			 */
-			public void onKeyUp(KeyUpEvent event) {
-				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					sendNameToServer();
-				}
-			}
-
-			/**
-			 * Send the name from the nameField to the server and wait for a response.
-			 */
-			private void sendNameToServer() {
-				// First, we validate the input.
-				errorLabel.setText("");
-				String textToServer = nameField.getText();
-				if (!FieldVerifier.isValidName(textToServer)) {
-					errorLabel.setText("Please enter at least four characters");
-					return;
-				}
-
-				// Then, we send the input to the server.
-				sendButton.setEnabled(false);
-				textToServerLabel.setText(textToServer);
-				serverResponseLabel.setText("");
-				greetingService.greetServer(textToServer,
-						new AsyncCallback<String>() {
-							public void onFailure(Throwable caught) {
-								// Show the RPC error message to the user
-								dialogBox
-										.setText("Remote Procedure Call - Failure");
-								serverResponseLabel
-										.addStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML(SERVER_ERROR);
-								dialogBox.center();
-								closeButton.setFocus(true);
-							}
-
-							public void onSuccess(String result) {
-								dialogBox.setText("Remote Procedure Call");
-								serverResponseLabel
-										.removeStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML(result);
-								dialogBox.center();
-								closeButton.setFocus(true);
-							}
-						});
-			}
-		}
-
-		// Add a handler to send the name to the server
-		MyHandler handler = new MyHandler();
-		sendButton.addClickHandler(handler);
-		nameField.addKeyUpHandler(handler);
+		// Atribui viibilidade para as tabs e adiciona ao painel da página
+		tabPanel.selectTab(0);
+		addPanel.add(tabPanel);
 	}
 }
