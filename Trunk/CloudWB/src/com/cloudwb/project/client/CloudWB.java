98cbbd11b3019ceb6eaeaec0238a49e4af6a7f35
thiagovaguiar@yahoo.com.br <thiagovaguiar@yahoo.com.br@bb2e0003-1184-2d7a-783e-2f35ea7fb7e2>
2010-10-01 02:29:51 +0000

    Pequenas modificações sem importância.

--- Trunk/CloudWB/src/com/cloudwb/project/client/CloudWB.java
+++ Trunk/CloudWB/src/com/cloudwb/project/client/CloudWB.java
@@ -23,8 +23,8 @@ public class CloudWB implements EntryPoint {
 		mainPanel.add(lastUpdatedLabel);
 
 		// Associate the Main panel with the HTML host page.
-		RootPanel.get("stockList").add(mainPanel);
-		
+		RootPanel.get("form").add(mainPanel);
+
 		// Create a tab panel
 		TabPanel tabPanel = new TabPanel();
 
@@ -55,7 +55,7 @@ public class CloudWB implements EntryPoint {
 		tabPanel.add(layoutText, "ESTRUTURA DO SITE");
 
 		// Adiciona Tab Template
-		HTML templantText = new HTML("Escolha de um Templante<br>"+
+		HTML templatText = new HTML("Escolha de um Template<br>"+
 				"<FORM METHOD=POST NAME=templante>" +
 				"<P>Opcoes de templante:</P>" +
 				"<P><INPUT TYPE=CHECKBOX NAME=op1>opcao 1" +
@@ -64,23 +64,20 @@ public class CloudWB implements EntryPoint {
 				"<INPUT TYPE=CHECKBOX NAME=op4>opcao 4</P><P>" +
 				"<INPUT TYPE=SUBMIT VALUE=SEGUIR ONCLICK=\"Javascript:alert('O form sera enviado!');\">" +
 				"<INPUT TYPE=RESET VALUE=LIMPAR></P>");
-		tabPanel.add(templantText, "TEMPLANTE");
+		tabPanel.add(templatText, "TEMPLATE");
 		
 		// Adiciona Tab Módulo
-		HTML modulText = new HTML("Formulario para a escolha dos modulos");
+		HTML modulText = new HTML("Formulario para a escolha dos modulos"+
+				"<FORM METHOD=POST NAME=nodulo>" +
+				"<P>Opcoes de modulos:</P>" +
+				"<P><INPUT TYPE=CHECKBOX NAME=op1>Graduacao" +
+				"<INPUT TYPE=CHECKBOX NAME=op2>Pos-graduacao</P>" +
+				"<P><INPUT TYPE=CHECKBOX NAME=op3>Mestrado" +
+				"<INPUT TYPE=CHECKBOX NAME=op4>Doutorado</P><P>" +
+				"<INPUT TYPE=SUBMIT VALUE=SEGUIR ONCLICK=\"Javascript:alert('O form sera enviado!');\">" +
+				"<INPUT TYPE=RESET VALUE=LIMPAR></P>");
 		tabPanel.add(modulText, "MODULOS");
 		
-		// Adiciona Exemplo
-		HTML exemplForm = new HTML("<FORM METHOD=POST NAME=contato>" +
-				" <P>Nome: <INPUT TYPE=TEXT NAME=USUARIO SIZE=10 MAXLENGTH=8></P>" +
-				"<P>Sobre quais assuntos voce gostaria de saber mais?</P>" +
-				"<P><INPUT TYPE=CHECKBOX NAME=ESPORTE>Esportes</P>" +
-				"<P><INPUT TYPE=CHECKBOX NAME=CULTURA>Cultura</P>" +
-				"<P><INPUT TYPE=CHECKBOX NAME=CULINARIA>Culinaria</P>" +
-				"<P><INPUT TYPE=CHECKBOX NAME=INFO>Informatica</P><P>" +
-				"<INPUT TYPE=SUBMIT VALUE=Enviar ONCLICK=\"Javascript:alert('O form sera enviado!');\">" +
-				"<INPUT TYPE=RESET VALUE=Limpar></P>");
-		tabPanel.add(exemplForm, "Exemplo Formulario");
 
 		// Atribui viibilidade para as tabs e adiciona ao painel da página
 		tabPanel.selectTab(0);
