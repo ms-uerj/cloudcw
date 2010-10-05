52d42b2a22f9b4e25374aa266322a79694b295fa
thiagovaguiar@yahoo.com.br <thiagovaguiar@yahoo.com.br@bb2e0003-1184-2d7a-783e-2f35ea7fb7e2>
2010-10-05 14:57:14 +0000

    Classe PaginaCLiente para criação do mesma.

---
+++ Trunk/CloudWB/src/com/cloudwb/project/client/PaginaCliente.java
@@ -0,0 +1,49 @@
+package com.cloudwb.project.client;
+
+public class PaginaCliente {
+  private String nomeSite;
+  private String tituloSite;
+  private String bannerSite;
+  private int[] moduloSite;
+  private int layoutSite;
+  
+  public String getNomeSite(){
+    return nomeSite;
+  }
+  
+  public void setNomeSite(String nome){
+    nomeSite = nome;
+  }
+  
+  public String getTituloSite(){
+	return tituloSite;
+  }
+  
+  public void setTituloSite(String titulo){
+	tituloSite = titulo;
+  }
+  
+  public String getBannerSite(){
+	return bannerSite;
+  }
+  
+  public void setBannerSite(String banner){
+    bannerSite = banner;
+  }
+  
+  public int getModuloSite( int i ){
+  	return moduloSite[i];    	
+  }
+
+  public void setModuloSite(){
+	
+  }
+  
+  public int getLayoutSite(){
+	return layoutSite;
+  }
+  
+  public void setLayoutSite(int layout){
+    layoutSite = layout;
+  }
+}
